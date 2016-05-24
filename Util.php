cd9eaa1a0e61ea805883d11a836f870c9f05e2f3
swan0 <swans.randomness@gmail.com>
2016-05-25 00:25:00 +0900

    バグ修正

--- Util.php
+++ Util.php
@@ -5,6 +5,9 @@ function execAllTest() {
     test1();
     test2();
     test3();
+    test4();
+    test5();
+    test6();
     test_exception1();
     test_exception2();
     test_exception3();
@@ -13,7 +16,7 @@ function execAllTest() {
 
 function test1() {
     // ARRANGE
-    $salt = 40;
+    $salt = 9;
     $probabilities = array(
         10,
         20,
@@ -25,7 +28,7 @@ function test1() {
     $result = Util::get_random($salt, $probabilities);
     
     // ASSERT
-    assertEquals(3, $result);
+    assertEquals(0, $result);
 }
 
 function test2() {
@@ -47,12 +50,61 @@ function test2() {
 
 function test3() {
     // ARRANGE
-    $salt = 100;
+    $salt = 99;
     $probabilities = array(
         10,
         20,
         20,
         50
+    );
+    
+    // ACT
+    $result = Util::get_random($salt, $probabilities);
+    
+    // ASSERT
+    assertEquals(3, $result);
+}
+
+function test4() {
+    // ARRANGE
+    $salt = 99;
+    $probabilities = array(
+        30,
+        70
+    );
+    
+    // ACT
+    $result = Util::get_random($salt, $probabilities);
+    
+    // ASSERT
+    assertEquals(1, $result);
+}
+
+function test5() {
+    // ARRANGE
+    $salt = 0;
+    $probabilities = array(
+        30,
+        70
+    );
+    
+    // ACT
+    $result = Util::get_random($salt, $probabilities);
+    
+    // ASSERT
+    assertEquals(0, $result);
+}
+
+function test6() {
+    // ARRANGE
+    $salt = 91;
+    $probabilities = array(
+        30,
+        10,
+        20,
+        30,
+        5,
+        5
     );
     
     // ACT
@@ -99,7 +151,7 @@ function test_exception2() {
         fail();
     } catch (Exception $e) {
         // ASSERT
-        assertEquals('不正な引数。$probabilitiesの総和は99。', $e->getMessage());
+        assertEquals('不正な引数。$probabilitiesの総和は100。', $e->getMessage());
     }
 }
 
@@ -119,8 +171,18 @@ function test_exception3() {
         fail();
     } catch (Exception $e) {
         // ASSERT
-        assertEquals('不正な引数。$probabilitiesの総和は99。', $e->getMessage());
+        assertEquals('不正な引数。$probabilitiesの総和は100。', $e->getMessage());
     }
+}
+
+function assertEquals($expect, $actual) {
+    if ($expect !== $actual) {
+        throw new Exception('assert fail.');
+    }
+}
+
+function fail() {
+    throw new Exception('Dont reach here.');
 }
 
 class Util
@@ -133,14 +195,22 @@ class Util
         if (!(0 <= $salt && $salt <= 99)) {
             throw new Exception('不正な引数。$saltの値は0～99。');
         }
-        if (array_sum($probabilities) != 99) {
-            throw new Exception('不正な引数。$probabilitiesの総和は99。');
+        if (array_sum($probabilities) != 100) {
+            throw new Exception('不正な引数。$probabilitiesの総和は100。');
         }
         $prevP = 0;
         foreach ($probabilities as $idx => $nextP) {
-            if ($prevP <= $salt && $salt < $prevP + $nextP) {
+            // echo 'Try :: ' . $idx . PHP_EOL;
+            // echo '$salt :: ' . $salt . PHP_EOL;
+            // echo '$prevP :: ' . $prevP . PHP_EOL;
+            // echo '$nextP :: ' . $nextP . PHP_EOL;
+            // echo '$prevP + $nextP :: ' . ($prevP + $nextP) . PHP_EOL;
+            // echo '___end___' . PHP_EOL;
+            
+            if ($prevP <= $salt && $salt < ($prevP + $nextP)) {
                 return $idx;
             }
+            $prevP += $nextP;
         }
     }
 }
